3fd247bab264a7ee669167211122fd4dc21705d4
Romano Studer <romano.studer@zitrone.com.au>
2026-04-23 11:43:02 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
+<<<<<<< Updated upstream
   return <div>Welcome to Pantheon Platform. This is my change 11. 12</div>;
+=======
+  return <div>Welcome to Pantheon Platform. This is my change number 10.</div>;
+>>>>>>> Stashed changes
 }
